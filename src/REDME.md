fa17e0bf9ec9a51907ff906a82486795fa950da2
tarikul-islam <tariikul12@gmail.com>
2023-04-28 21:14:08 +0600

    user React Bootstrap

--- src/REDME.md
+++ src/REDME.md
@@ -2,3 +2,7 @@
 
 ## For Time Dynamic Set
 see documentation [moment Js](https://momentjs.com/)
+
+
+## React FAST Marquee
+see documentation [Marquee](https://www.react-fast-marquee.com/)
--- src/pages/Home/Shared/Header/Header.jsx
+++ src/pages/Home/Shared/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from '../../../../assets/logo.png'
 import moment from 'moment';
-import { Container,Button } from 'react-bootstrap';
+import { Container,Button, Navbar, Nav } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 const Header = () => {
     return (
         <Container>
@@ -10,7 +11,30 @@ const Header = () => {
             <p>Journalism Without Fear or Favour</p>
             <p>{moment().format("dddd, MMMM D YYYY")}</p>
            </div>
+           <div className='d-flex '>
            <Button variant="danger">Latest</Button>
+           <Marquee className='text-danger' speed={200} gradientColor={[2, 255, 25]}>
+  I can be a React component, multiple React components, or just some text.
+</Marquee>
+           </div>
+           <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">Career</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Profile</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+            <Button variant="secondary">Secondary</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         </Container>
     );
 };
