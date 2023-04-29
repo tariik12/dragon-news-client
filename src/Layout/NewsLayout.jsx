88ee7acba091cef5aff38dbc5fed4277919ae663
tarikul-islam <tariikul12@gmail.com>
2023-04-29 08:31:27 +0600

    news layout done

--- src/Layout/NewsLayout.jsx
+++ src/Layout/NewsLayout.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import Header from '../pages/Home/Shared/Header/Header';
+import Footer from '../pages/Home/Shared/Footer/Footer';
+import { Col, Container, Row } from 'react-bootstrap';
+import LeftNav from '../pages/Home/Shared/LeftNav/LeftNav';
+import RightNav from '../pages/Home/Shared/RightNav/RightNav';
+import { Outlet } from 'react-router-dom';
 
 const NewsLayout = () => {
     return (
         <div>
-            
+            <Header></Header>
+        <Container>
+      <Row>
+        
+        <Col lg={9}>
+           <Outlet />
+        </Col>
+        <Col lg={3}>
+            <RightNav></RightNav>
+        </Col>
+      </Row>
+    </Container>
+        <Footer></Footer>
         </div>
     );
 };
--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Category from "../pages/Home/Category/Category";
+import NewsLayout from "../Layout/NewsLayout";
+import News from "../pages/Home/News/News";
 
 
 const router = createBrowserRouter([
@@ -16,6 +18,16 @@ const router = createBrowserRouter([
         {
             path:'/category/:id',
             element:<Category />
+        }
+    ]
+},
+{
+    path:'news',
+    element:<NewsLayout />,
+    children:[
+        {
+         path:':id',
+         element:<News />   
         }
     ]
 }
